0076f744513e4fd5272e23c0c45504f863c2f0db
Titouan Vervack <titouan.vervack@sigasi.com>
2017-04-07 17:37:02 +0200

    Fix run script

--- iCEstick/run.sh
+++ iCEstick/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ghdl -a leds.vhd
-yosys -m ~/git/ghdlsynth-beta/ghdl.so -p 'ghdl leds; synth_ice40 -blif leds.blif'
+yosys -m /vagrant/git/ghdlsynth-beta/ghdl.so -p 'ghdl leds; synth_ice40 -blif leds.blif'
 arachne-pnr -d 1k -o leds.asc -p leds.pcf leds.blif
 icepack leds.asc leds.bin
-iceprog leds.bin
+sudo /usr/local/bin/iceprog leds.bin

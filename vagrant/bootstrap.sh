cc1d9f061a7cdad17341a581fd42d7ff6d220b4e
Hendrik Eeckhaut <hendrik.eeckhaut@sigasi.com>
2017-04-07 17:05:23 +0200

    Fixes

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -5,43 +5,51 @@ dnf -y update
 sudo dnf -y install make automake gcc gcc-c++ kernel-devel clang bison \
                  flex readline-devel gawk tcl-devel libffi-devel git mercurial \
                  graphviz python-xdot pkgconfig python python3 libftdi-devel \
-                 gcc-gnat 
+                 gcc-gnat usbutils
 dnf clean all
 
+echo "Sigasi" | sudo passwd vagrant --stdin
+
 #Installing the IceStorm Tools (icepack, icebox, iceprog, icetime, chip databases):
+mkdir git
+pushd git
 
 git clone https://github.com/cliffordwolf/icestorm.git icestorm; \
-    cd icestorm; \
+    pushd icestorm; \
     make -j$(nproc); \
-    sudo make install;
+    sudo make install; \
+    popd;
 
 # Installing Arachne-PNR (the place&route tool):
 
 git clone https://github.com/cseed/arachne-pnr.git arachne-pnr; \
-	cd arachne-pnr; \
+	pushd arachne-pnr; \
 	make -j$(nproc); \
-	sudo make install;
+	sudo make install; \
+    popd;
 
 # Installing Yosys (Verilog synthesis):
 
 git clone https://github.com/cliffordwolf/yosys.git yosys; \
-	cd yosys; \
+	pushd yosys; \
 	make -j$(nproc); \
-	sudo make install;
+	sudo make install; \
+    popd;
 
 # GHDL
 
 git clone https://github.com/tgingold/ghdl.git ghdl; \
-    cd ghdl; \
+    pushd ghdl; \
     ./configure --prefix=/usr/local; \
     make -j$(nproc); \
     sudo make install; \
     make libghdlsynth.so; \
-    sudo make install.libghdlsynth.shared;
+    sudo make install.libghdlsynth.shared; \
+    popd;
 
 git clone https://github.com/tgingold/ghdlsynth-beta.git ghdlsynth-beta; \
-    cd ghdlsynth-beta; \
-    make GHDL_PREFIX=../ghdl; 
+    pushd ghdlsynth-beta; \
+    make GHDL_PREFIX=/usr/local; \
+    popd
 
-
-
+popd
